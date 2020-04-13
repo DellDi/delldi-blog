aaff0bfd51c34ee258d96837c225e83be2f66605
“ <875372314@qq.com>
2020-04-13 20:05:07 +0800

    update: md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ module.exports = {
         // },
         blog: {
           path: './blog',
-          routeBasePath: '/',
+          routeBasePath: '/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
